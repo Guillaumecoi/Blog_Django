b9e7e0fa8f8a3113191425a5dbfd70d3d7f9057c
Guillaume <coigniez.g@gmail.com>
2024-03-10 11:07:14 +0100

    upload authenticate api

---
+++ SessionBased_Authentication_api/djangoserver/users/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model, authenticate
+
+UserModel = get_user_model()
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = UserModel
+        fields = ["email", "password"] 
+    
+    def create(self, validated_data):
+        if 'email' not in validated_data:
+            raise serializers.ValidationError("The email field is required.")
+        validated_data['username'] = validated_data['email']
+        user = UserModel.objects.create_user(**validated_data)
+        return user
+    
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(username=data['email'], password=data['password'])
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Incorrect Credentials")
+    
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = ["id", "email"]
